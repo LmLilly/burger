baf3927c875094eadb7a3744248c1ebc09c58dcc
LmLilly <lillym@Lidiettes-MacBook-Pro.local>
2017-07-28 19:05:30 -0700

    Burger app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', 1);
+INSERT INTO burgers (burger_name) VALUES ('Beef Burger', 0);
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger', 1);
